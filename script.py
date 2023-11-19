80616753712463b57195ddd5df8a14f343ae105a
jorgensd <dokken92@gmail.com>
2023-11-19 20:03:46 +0100

    Various updates

--- script.py
+++ script.py
@@ -1,72 +1,69 @@
-import condastats.cli
-import pypistats
-import pandas
-import json
-import subprocess
-import sys
-import warnings
+import argparse
 import datetime
+import json
 import math
-owner = sys.argv[1]
-repo = sys.argv[2]
-conda_name = sys.argv[3]
-pypi_name = sys.argv[4]
+import subprocess
+import warnings
 
-clones = subprocess.run([
-    'gh', 'api', "-H", "Accept:application/vnd.github+json",   "-H", "X-GitHub-Api-Version:2022-11-28",
-    f'/repos/{owner}/{repo}/traffic/clones'], capture_output=True
-)
-views = subprocess.run([
-    'gh', 'api', "-H", "Accept:application/vnd.github+json",   "-H", "X-GitHub-Api-Version:2022-11-28",
-    f'/repos/{owner}/{repo}/traffic/views'], capture_output=True
-)
+import condastats.cli
+import pandas
+import pypistats
 
-repo_clones = json.loads(clones.stdout)
-repo_views = json.loads(views.stdout)
+_date_format = '%d-%m-%Y'
 
 
-date_format = '%d-%m-%Y'
-for key, infile in zip(["clones", "views"], [repo_clones, repo_views]):
-    df = pandas.DataFrame.from_dict(infile[key])
-    unique = infile["uniques"]
-
-    dates = pandas.to_datetime(df["timestamp"])
-    start_date = pandas.to_datetime(dates).sort_values().iloc[0]
-    end_date = pandas.to_datetime(dates).sort_values().iloc[-1]
-    print("-"*25)
-    print(
-        f"Github traffic review of https://www.github.com/{owner}/{repo} from {start_date.strftime(date_format)} to {end_date.strftime(date_format)}")
-    print(f"Number of unique {key}: {unique}")
-    num_days = (end_date-start_date).days
-
-    if len(dates) != num_days+1:
-        num_days = len(dates)
-        warnings.warn("We do not have data for every day")
-    print(
-        f"Average unqiue vistors per day (can be duplicated over multiple days): {math.floor(df['uniques'].sum()/len(df['uniques']))}")
-
-if conda_name != "None":
-    stats = condastats.cli.overall(conda_name, monthly=True)[conda_name]
+def get_conda_stats(name: str):
+    stats = condastats.cli.overall(name, monthly=True)[name]
     conda_dates = pandas.Series(pandas.to_datetime(stats.index).sort_values())
     start_month = conda_dates.iloc[0]
     end_month = conda_dates.iloc[-1]
     month_format = "%m-%Y"
-    print("-"*25)
     print(
         f"Monthly conda stats from {start_month.strftime(month_format)} to {end_month.strftime(month_format)}")
     print(f"Total downloads: {stats.sum()}")
     print(f"Average per month: {math.floor(stats.mean())}")
 
 
-if pypi_name != "None":
-    with_mirrors = True
-    if with_mirrors:
+def get_github_stats(repository: str):
+    clones = subprocess.run([
+        'gh', 'api', "-H", "Accept:application/vnd.github+json",   "-H", "X-GitHub-Api-Version:2022-11-28",
+        f'/repos/{repository}/traffic/clones'], capture_output=True
+    )
+    views = subprocess.run([
+        'gh', 'api', "-H", "Accept:application/vnd.github+json",   "-H", "X-GitHub-Api-Version:2022-11-28",
+        f'/repos/{repository}/traffic/views'], capture_output=True
+    )
+
+    repo_clones = json.loads(clones.stdout)
+    repo_views = json.loads(views.stdout)
+
+    for key, infile in zip(["clones", "views"], [repo_clones, repo_views]):
+        df = pandas.DataFrame.from_dict(infile[key])
+        unique = infile["uniques"]
+
+        dates = pandas.to_datetime(df["timestamp"])
+        start_date = pandas.to_datetime(dates).sort_values().iloc[0]
+        end_date = pandas.to_datetime(dates).sort_values().iloc[-1]
+        print(
+            f"Github traffic review of https://www.github.com/{repository} from {start_date.strftime(_date_format)} to {end_date.strftime(_date_format)}")
+        print(f"Number of unique {key}: {unique}")
+        num_days = (end_date-start_date).days
+
+        if len(dates) != num_days+1:
+            num_days = len(dates)
+            warnings.warn("We do not have data for every day")
+        print(
+            f"Average unqiue vistors per day (can be duplicated over multiple days): {math.floor(df['uniques'].sum()/len(df['uniques']))}")
+
+
+def get_pypi_stats(name: str):
+    if args.with_mirrors:
         sub_key = "with_mirrors"
     else:
         sub_key = "without_mirrors"
 
     pypi_json = pypistats.overall(
-        pypi_name, total="monthly", format="json")
+        args.pypi, total="monthly", format="json")
 
     pypi_stats = json.loads(pypi_json)["data"]
     accumulate_stats = {"dates": [], "downloads": []}
@@ -79,9 +76,39 @@ if pypi_name != "None":
     pypi_df = pandas.DataFrame.from_dict(accumulate_stats)
     start_date = pypi_df["dates"].sort_values().iloc[0]
     end_date = pypi_df["dates"].sort_values().iloc[-1]
-    print("-"*25)
+
     print(
-        f"PYPI: https://pypi.org/project/{pypi_name} ({sub_key}) from {start_date.strftime(date_format)} to {end_date.strftime(date_format)}")
+        f"PYPI: https://pypi.org/project/{args.pypi} ({sub_key}) from {start_date.strftime(_date_format)} to {end_date.strftime(_date_format)}")
     print(f"Total downloads: {pypi_df['downloads'].sum()}")
     print(
         f"Monthly average downloads: {math.floor(pypi_df['downloads'].mean())}")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--github", type=str,
+                        dest="github", default=None,
+                        help="Github location, format 'user/repository'")
+    parser.add_argument("--conda", type=str,
+                        dest="conda", default=None,
+                        help="Name of conda package")
+    parser.add_argument("--pypi", type=str,
+                        dest="pypi", default=None,
+                        help="Name of pypi package")
+    parser.add_argument("--without-mirrors", action="store_false",
+                        default=True, dest="with_mirrors",
+                        help="If getting pypi images, decide if you want to get info with or without mirrors")
+    args = parser.parse_args()
+    if args.github is not None:
+        get_github_stats(args.github)
+        print("-"*25)
+
+    if args.conda is not None:
+        get_conda_stats(args.conda)
+        print("-"*25)
+
+    if args.pypi is not None:
+        get_pypi_stats(args.pypi)
+        print("-"*25)
